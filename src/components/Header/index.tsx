144f3a71db596c595c5ac14333ddff4eb6b1a155
ramonis567 <ramonmatosferreira@gmail.com>
2021-11-12 17:58:02 -0300

    header style ok

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
+import logo from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
 export  function Header() {
     return (
-        <header>
-            Header
-        </header>
+        <Container>
+            <Content> 
+                <img src={logo} alt="dtmoney logo" />
+                <button type="button">
+                    Nova transação
+                </button>
+            </Content>
+        </Container>
     )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    background: var(--blue);
+
+`
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2rem 1rem 12rem; //0 16px 160px
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+        font-size: 1rem;
+        color: var(--title);
+        background: #019990;
+        border: 0;
+        border-radius: 0.25rem;
+        padding: 0.5rem 1.75rem;
+        height: 3rem;
+        transition: 0.3s all;
+
+        &:hover {
+            filter: brightness(0.8);
+            filter: sepia(0.3);
+            transform: scale(1.01);
+        }
+    }
+`
