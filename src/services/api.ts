7b92cabed79216c815e3635dbc047e403f72f21c
Ramon Ferreira Matos <ramonmatosferreira@gmail.com>
2021-11-26 16:15:00 -0300

    Update api.ts
    Changed local fake api to remote

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    // baseURL: 'http://localhost:3000/api',   local
+    baseURL: 'dtmoney-ramonis567.vercel.app/api', // remoto
 })
